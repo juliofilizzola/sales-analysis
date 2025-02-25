155fdd5e32c76ada249711dd0562ced3f5d70a5f
Julio Filizzola <juliofilizzola@hotmail.com>
2025-02-25 14:15:21 -0300

    Add analyzer module for calculating sales metrics

---
+++ scripts/analyzer.py
@@ -0,0 +1,8 @@
+def calculate_metric(data):
+    metric = {
+        'total_sales': data['total_sale'].sum(),
+        'average_sales_per_day': data.groupby('date')['total_sale'].sum().mean(),
+        'most_sold_product': data['product'].value_counts().idxmax(),
+        'city_with_highest_sales': data.groupby('city')['total_sale'].sum().idxmax()
+    }
+    return metric
